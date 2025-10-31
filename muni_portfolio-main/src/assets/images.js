3db192cc5dd41df3cbe7841ca2030140d785c604
Poovesh V <poovesh057@gmail.com>
2025-10-31 13:20:26 +0530

    profile update

--- muni_portfolio-main/src/assets/images.js
+++ muni_portfolio-main/src/assets/images.js
@@ -5,7 +5,7 @@ import todolist from "./todo.png";
 import redux from "./redux.webp";
 import weather from "./weather.webp";
 import pooveshContact from "./poovesh-contact.webp";
-import poovesh from "./poovesh.webp";
+import poovesh from "./poovesh.webp"
 import topRight from "./top-right.webp";
 
 export {
